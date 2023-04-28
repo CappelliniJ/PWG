4203d81da085901a272ced76073c492003a14568
Justin Cappellini <cappellinij@gmail.com>
2023-04-28 21:16:31 +0800

    added a database

--- public/script.js
+++ public/script.js
@@ -1,10 +1,12 @@
-function generatePassword(length, lowercase, uppercase, number, special) {
-  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numberChars = "0123456789";
-  const specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+const db = require('./db.js');
 
-  let allChars = "";
+async function generatePassword(length, lowercase, uppercase, number, special) {
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberChars = '0123456789';
+  const specialChars = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+
+  let allChars = '';
 
   if (lowercase) {
     allChars += lowercaseChars;
@@ -23,37 +25,25 @@ function generatePassword(length, lowercase, uppercase, number, special) {
   }
 
   if (allChars.length === 0) {
-    return "Error: At least one character set must be selected.";
+    return 'Error: At least one character set must be selected.';
   }
 
   if (length < 4 || length > 128) {
-    return "Error: Password length must be between 4 and 128.";
+    return 'Error: Password length must be between 4 and 128.';
   }
 
-  let password = "";
-  for (let i = 0; i < length; i++) {
-    password += allChars.charAt(Math.floor(Math.random() * allChars.length));
+  let password = '';
+  let passwordExists = true;
+
+  while (passwordExists) {
+    password = '';
+    for (let i = 0; i < length; i++) {
+      password += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    }
+    passwordExists = await db.checkIfPasswordExists(password);
   }
+
+  await db.insertPassword(password);
 
   return password;
 }
-
-const generateButton = document.getElementById("generate");
-const resultDiv = document.getElementById("result");
-
-generateButton.addEventListener("click", function () {
-  const length = document.getElementById("length").value;
-  const lowercase = document.getElementById("lowercase").checked;
-  const uppercase = document.getElementById("uppercase").checked;
-  const numbers = document.getElementById("numbers").checked;
-  const special = document.getElementById("special").checked;
-
-  const password = generatePassword(
-    length,
-    lowercase,
-    uppercase,
-    numbers,
-    special
-  );
-  resultDiv.textContent = password;
-});
